28e25bcd6a5a26593665df20d9bfabcc1acc5ad2
Agustín Degano <adegano@hotmail.com.ar>
2024-11-13 11:01:20 -0300

    [ADD] Se agregó la pagina de Contacto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { EmpresasComponent } from './empresas/empresas.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 export const routes: Routes = [
     {path: 'empresas', component: EmpresasComponent },
-    {path: 'inicio', component: InicioComponent }
+    {path: 'inicio', component: InicioComponent },
+    {path: 'contacto', component: ContactoComponent}
 ];
 
 @NgModule({
