8b0555060f71ff2b2e882ffb77037bdd5fb30524
Agustín Degano <adegano@hotmail.com.ar>
2024-11-13 17:49:58 -0300

    [UPDATE] Se agregaron mas funcionalidades a la pagina principal. (Contiene errores)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,19 @@ import { EmpresasComponent } from './empresas/empresas.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { ContactoComponent } from './contacto/contacto.component';
 
+
 export const routes: Routes = [
     {path: 'empresas', component: EmpresasComponent },
     {path: 'inicio', component: InicioComponent },
-    {path: 'contacto', component: ContactoComponent}
+    {path: 'contacto', component: ContactoComponent},
+    {path: '', component: InicioComponent },
+
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [
+        RouterModule.forRoot(routes),
+    ],
     exports: [RouterModule]
   })
   export class AppRoutingModule { }
--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+import { NgbTimepickerModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [NgbTimepickerModule, FormsModule, JsonPipe],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
-
+  time = { hour: 13, minute: 30 }
 }
+
