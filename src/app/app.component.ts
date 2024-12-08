14d6c2b6b1e1b5604bdf02a7145b36a65c64050b
Agustín Degano <adegano@hotmail.com.ar>
2024-12-08 13:28:49 -0300

    [Add] Se agregaron elementos para la conxión con el frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BaseHttpService } from './service/base-http-service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private httpClientService: BaseHttpService) { }
+  ngOnInit(): void {
+    this.httpClientService.getData().subscribe((result:string) => {
+      console.log(result)
+    })
+  }
   title = 'frontend';
 }
